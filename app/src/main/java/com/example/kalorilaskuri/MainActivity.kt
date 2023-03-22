5036d0f286121574105e24a0b5f423d5d5bae82c
ArttMyll <arttu.myllari@edu.savonia.fi>
2023-03-22 17:39:00 +0200

    Arttu kommenti

--- app/src/main/java/com/example/kalorilaskuri/MainActivity.kt
+++ app/src/main/java/com/example/kalorilaskuri/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // Antti lisäsi kommentin
+        // Arttu Lisäsi kommentin
     }
 }
