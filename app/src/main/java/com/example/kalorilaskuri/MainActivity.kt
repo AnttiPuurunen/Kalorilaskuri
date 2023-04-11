b0cbf7342cfb5dbff9ec7b8fe16c6d63e078d198
Kimmo79 <105000509+Kimmo79@users.noreply.github.com>
2023-04-11 11:27:54 +0300

    data siirretty tarkastele sivulle

--- app/src/main/java/com/example/kalorilaskuri/MainActivity.kt
+++ app/src/main/java/com/example/kalorilaskuri/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.kalorilaskuri
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kalorilaskuri.databinding.FragmentStartBinding
+import com.example.kalorilaskuri.viewmodels.MealViewModel
+import com.example.kalorilaskuri.viewmodels.MealViewModelFactory
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+
 
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
