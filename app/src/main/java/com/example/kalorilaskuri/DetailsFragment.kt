5dd00c06e2ca231b0f2af4ff103f0605dce2575c
AnttiPuurunen <antti.puurunen@edu.savonia.fi>
2023-05-08 17:10:40 +0300

    Pieniä muokkauksia

--- app/src/main/java/com/example/kalorilaskuri/DetailsFragment.kt
+++ app/src/main/java/com/example/kalorilaskuri/DetailsFragment.kt
@@ -24,6 +24,7 @@ class DetailsFragment : Fragment() {
 
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
+    private var emptyList: Boolean = false
     private val mealViewModel: MealViewModel by activityViewModels {
         MealViewModelFactory(
             (activity?.application as CalorieApplication).database.mealDao()
@@ -45,6 +46,7 @@ class DetailsFragment : Fragment() {
         binding.apply {
             lifecycleOwner = viewLifecycleOwner
             var detailsFragment = this@DetailsFragment
+            emptyList = emptyList
         }
 
         val prefs = requireActivity().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
@@ -125,16 +127,18 @@ class DetailsFragment : Fragment() {
         mealViewModel.mealsByDate.removeObservers(this)
         mealViewModel.mealsByDate.observe(this.viewLifecycleOwner) { items ->
             items.let {
-                if (it.isEmpty()) {
+                if (it.isNullOrEmpty()) {
                     binding.apply {
                         emptyListTextview.visibility = View.VISIBLE
                         recyclerView.visibility = View.GONE
+                        emptyList = true
                     }
                 } else if (chosenDate == "") {
                     adapter.submitList(it.distinctBy { it.date })
                     binding.apply {
                         emptyListTextview.visibility = View.GONE
                         recyclerView.visibility = View.VISIBLE
+                        emptyList = false
                     }
                 } else {
                     val list = it.filter { it.date == chosenDate }
@@ -142,6 +146,7 @@ class DetailsFragment : Fragment() {
                     binding.apply {
                         emptyListTextview.visibility = View.GONE
                         recyclerView.visibility = View.VISIBLE
+                        emptyList = false
                     }
                 }
             }
